b489b17c33038ba26712fc2fafffa59f96e64d27
minwg1021 <minwg1021@gmail.com>
2023-06-09 18:44:23 +0900

    part2 코드 수정 및 서식변경

--- api/api/main.py
+++ api/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from part1 import part1_router
-from part2 import strangeSentence
+from part2 import part2_router
 
 app = FastAPI()
 
 app.include_router(part1_router.router)
-app.include_router(strangeSentence.router)
+app.include_router(part2_router.router)
