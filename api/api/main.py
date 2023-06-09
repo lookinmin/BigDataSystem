bf7b4689914a1f34b715c705f33e083f818d8790
minwg1021 <minwg1021@gmail.com>
2023-06-09 17:44:40 +0900

    part2 → 원문과 어울리지않는 문장 찾기 추가

--- api/api/main.py
+++ api/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from part1 import OX
-from part2 import temp
+from part2 import strangeSentence
 
 app = FastAPI()
 
 app.include_router(OX.router)
-app.include_router(temp.router)
+app.include_router(strangeSentence.router)
---
+++ api/api/part2/strangeSentence.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter
+from pymongo import MongoClient
+import pprint
+
+router = APIRouter(prefix="/part2/strangeSentence")
+
+
+@router.get("/")
+async def root():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["BDS"]
+    collection = db["BDS"]
+    list_results = []
+
+    query = [
+        {
+            "$match": {
+                "sourceDataInfo.newsCategory": "경제",
+                "sourceDataInfo.processLevel": "하",
+                "sourceDataInfo.partNum": "P2",
+            }
+        },
+        {"$sample": {"size": 3}},
+        {
+            "$project": {
+                "_id": 0,
+                "sourceDataInfo.newsTitle": 1,
+                "labeledDataInfo.processSentenceInfo": 1,
+            }
+        },
+    ]
+
+    results = collection.aggregate(query)
+    for result in results:
+        list_results.append(result)
+
+    return list_results
