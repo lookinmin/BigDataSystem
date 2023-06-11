855a5c0b14f297bf4c8a0cad2fc141efa205cd8f
JongWon <3338761@naver.com>
2023-06-11 15:23:47 +0900

    <feat> : graph 리턴값 구조화 --------------------------------- 1. graph 리턴값 구조화

--- api/api/graph/graph_impl.py
+++ api/api/graph/graph_impl.py
@@ -15,6 +15,11 @@ def read_statistics():
 #카테고리 및 난이도 별 개수
     query1=[
     {
+        '$sort': {
+            'sourceDataInfo.newsCategory': 1, 
+            'sourceDataInfo.processLevel': 1
+        }
+    }, {
         '$group': {
             '_id': [
                 '$sourceDataInfo.newsCategory', '$sourceDataInfo.processLevel'
@@ -42,7 +47,7 @@ def read_statistics():
             '_id': [
                 '$sourceDataInfo.partNum', '$sourceDataInfo.processPattern'
             ], 
-            'fieldN': {
+            'count': {
                 '$sum': 1
             }
         }
@@ -56,11 +61,40 @@ def read_statistics():
     coll=conn_COLL()
     return_data=[]
     CL_data=[]
-    TP_data=[]
-    for i in coll.aggregate(query1):
-        CL_data.append(i)
+    temp_dic={}
+    for idx,value in enumerate(coll.aggregate(query1),1):
+        if idx%3==0:
+            temp_dic[value.get("_id")[1]]=value.get("count")
+            CL_data.append({"key":value.get("_id")[0],"value":temp_dic})
+            temp_dic={}
+        else:
+            temp_dic[value.get("_id")[1]]=value.get("count")
+
+    temp_dic=[{"key":"제목과 본문의 불일치 기사","value":{}},{"key":"본문의 도메인 일관성 부족 기사","value":{}}]
     for i in coll.aggregate(query2):
-        TP_data.append(i)
-    return_data.append({"CL_count":CL_data,"TP_count":TP_data})
+        id=i.get("_id")
+        if id[0]=="P1":
+            if id[1]=="11":
+                temp_dic[0]["value"]["의문 유발형(부호)"]=i.get("count")
+            elif id[1]=="12":
+                temp_dic[0]["value"]["의문 유발형(은닉)"]=i.get("count")
+            elif id[1]=="13":
+                temp_dic[0]["value"]["선정표현 사용형"]=i.get("count")
+            elif id[1]=="14":
+                temp_dic[0]["value"]["속어/줄임말 사용형"]=i.get("count")
+            elif id[1]=="15":
+                temp_dic[0]["value"]["사실 과대 표현형"]=i.get("count")
+            elif id[1]=="16":
+                temp_dic[0]["value"]["의도적 주어 왜곡형"]=i.get("count")
+        else:
+            if id[1]=="21":
+                temp_dic[1]["value"]["상품 판매정보 노출 광고형"]=i.get("count")
+            elif id[1]=="22":
+                temp_dic[1]["value"]["부동산 판매정보 노출 광고형"]=i.get("count")
+            elif id[1]=="23":
+                temp_dic[1]["value"]["서비스 판매정보 노출 광고형"]=i.get("count")
+            elif id[1]=="24":
+                temp_dic[1]["value"]["의도적 상황 왜곡/전환형"]=i.get("count")
+    return_data.append({"CL_count":CL_data,"TP_count":temp_dic})
 
     return return_data
